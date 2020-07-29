8d553d02835ae0590d560aa80b60e216d817414e
samuelmachado <samuel.silva39@fatec.sp.gov.br>
2020-07-29 06:57:00 -0300

    following system

--- app/User.php
+++ app/User.php
@@ -45,4 +45,14 @@ class User extends Authenticatable
     {
         return Tweet::where('user_id', $this->id)->latest()->get();
     }
+
+    public function follow($user)
+    {
+        return $this->follows()->save($user);
+    }
+    public function follows()
+    {
+        return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
+    }
+    
 }
--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,10 +1,10 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 <ul>
-    @foreach(range(1,8) as $index)
+    @foreach(auth()->user()->follows as $user)
         <li class="mb-4">
             <div class="flex items-center text-sm">
-                <img src="https://i.pravatar.cc/40" class="rounded-full mr-2" alt="">
-                John Doe
+                <img src="{{$user->avatar}}" class="rounded-full mr-2" alt="">
+                {{$user->name}}
             </div>
         </li>
     @endforeach
